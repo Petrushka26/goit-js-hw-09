f7e127d20dc3ba9fb8a47ddfc8a8ebd59f4ea52e
Oleksandr <112812814+Petrushka26@users.noreply.github.com>
2022-12-01 21:35:02 +0200

    ready for chek

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,37 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+const formIn = document.querySelector('.form');
+
+formIn.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  const { delay, step, amount } = event.target.elements;
+  let delayVal = Number(delay.value);
+  const stepVal = Number(step.value);
+  const amountVal = Number(amount.value);
+
+  for (let i = 1; i <= amountVal; i += 1) {
+    createPromise(i, delayVal)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    delayVal += stepVal;
   }
 }
