4588ca495ef45b12339e3d52ae5dc2f610d00076
Petrushka26 <112812814+Petrushka26@users.noreply.github.com>
2023-05-21 12:29:17 +0000

    Deploying to gh-pages from @ Petrushka26/goit-js-hw-09@f956eab3ea198c0ffb6747f70b78861ea6fb8143 🚀

---
+++ 01-color-switcher.4985743a.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),r=document.querySelector("body");let o=null;t.addEventListener("click",(function(d){o=setInterval((()=>{console.log(o),r.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),t.setAttribute("disabled","true"),e.removeAttribute("disabled")})),e.addEventListener("click",(function(r){clearInterval(o),t.removeAttribute("disabled"),e.setAttribute("disabled","true")})),e.setAttribute("disabled","true");
+//# sourceMappingURL=01-color-switcher.4985743a.js.map
